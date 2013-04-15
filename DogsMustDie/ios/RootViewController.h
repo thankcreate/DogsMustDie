1db565b43dd8e1614dd2996fe938f27e1dd21c3b
ThankCreate <thankcreate@gmail.com>
2013-04-15 22:53:58 +0800

    init commit

---
+++ DogsMustDie/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  DogsMustDieAppController.h
+//  DogsMustDie
+//
+//  Created by Tron Skywalker on 13-4-15.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
---
+++ DogsMustDie/ios/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  DogsMustDie
+//
+//  Created by Tron Skywalker on 13-4-15.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
